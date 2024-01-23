73be9c4d02e642e27df990484162e78b0fd8c7cd
Felix Sinne <felix.sinne.extern@axelspringer.com>
2024-01-23 20:40:22 +0100

    docker compose FE and BE

--- favorite-songs/src/main/resources/data.sql
+++ favorite-songs/src/main/resources/data.sql
@@ -1,110 +1,108 @@
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre) VALUES ('2d68560b-d925-40bb-9cd5-93a75710d0dd', 'Shot ya', 'Beyonce', 'World', 1999, 180, 'Soul');
----- Song 2
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  'c6d69963-62c2-4df2-8bc8-9b333f9d5d1d',
---  'Moonlight Sonata',
---  'Ludwig van Beethoven',
---  'Classical Hits',
---  1800,
---  300,
---  'Classical'
---);
---
----- Song 3
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  'a5e27995-33d3-4f37-bd7a-c1d8d1ac598f',
---  'Bohemian Rhapsody',
---  'Queen',
---  'A Night at the Opera',
---  1975,
---  354,
---  'Rock'
---);
---
----- Song 4
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  '0aef7493-1243-417a-9f8b-0a96f08a481d',
---  'Shape of You',
---  'Ed Sheeran',
---  '÷',
---  2017,
---  234,
---  'Pop'
---);
---
----- Song 5
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  'bd2ebd6e-f0c5-4c71-98e2-b0fd1860a764',
---  'Billie Jean',
---  'Michael Jackson',
---  'Thriller',
---  1982,
---  294,
---  'Pop'
---);
---
----- Song 6
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  '2c2568c9-6aeb-4872-8c79-5cf260791524',
---  'Hotel California',
---  'Eagles',
---  'Hotel California',
---  1976,
---  390,
---  'Rock'
---);
---
----- Song 7
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  'f8b4b811-58f1-4d5a-8a93-2a0138a144d4',
---  'Imagine',
---  'John Lennon',
---  'Imagine',
---  1971,
---  182,
---  'Rock'
---);
---
----- Song 8
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  '1c8fe2af-15c3-4f8c-a688-7e91ab3b22ce',
---  'Rolling in the Deep',
---  'Adele',
---  '21',
---  2010,
---  228,
---  'Pop'
---);
---
----- Song 9
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  'f02e73c3-d6bb-4e4b-b9ec-731276c69f0d',
---  'Stairway to Heaven',
---  'Led Zeppelin',
---  'Led Zeppelin IV',
---  1971,
---  482,
---  'Rock'
---);
---
----- Song 10
---INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
---VALUES (
---  '320ef126-df23-4295-8cf9-74d50e4a5db7',
---  'I Will Always Love You',
---  'Whitney Houston',
---  'The Bodyguard',
---  1992,
---  273,
---  'Pop'
---);
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre) VALUES ('2d68560b-d925-40bb-9cd5-93a75710d0dd', 'Shot ya', 'Beyonce', 'World', 1999, 180, 'Soul');
+-- Song 2
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  'c6d69963-62c2-4df2-8bc8-9b333f9d5d1d',
+  'Moonlight Sonata',
+  'Ludwig van Beethoven',
+  'Classical Hits',
+  1800,
+  300,
+  'Classical'
+);
 
-select * from song;
+-- Song 3
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  'a5e27995-33d3-4f37-bd7a-c1d8d1ac598f',
+  'Bohemian Rhapsody',
+  'Queen',
+  'A Night at the Opera',
+  1975,
+  354,
+  'Rock'
+);
+
+-- Song 4
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  '0aef7493-1243-417a-9f8b-0a96f08a481d',
+  'Shape of You',
+  'Ed Sheeran',
+  '÷',
+  2017,
+  234,
+  'Pop'
+);
+
+-- Song 5
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  'bd2ebd6e-f0c5-4c71-98e2-b0fd1860a764',
+  'Billie Jean',
+  'Michael Jackson',
+  'Thriller',
+  1982,
+  294,
+  'Pop'
+);
+
+-- Song 6
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  '2c2568c9-6aeb-4872-8c79-5cf260791524',
+  'Hotel California',
+  'Eagles',
+  'Hotel California',
+  1976,
+  390,
+  'Rock'
+);
+
+-- Song 7
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  'f8b4b811-58f1-4d5a-8a93-2a0138a144d4',
+  'Imagine',
+  'John Lennon',
+  'Imagine',
+  1971,
+  182,
+  'Rock'
+);
+
+-- Song 8
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  '1c8fe2af-15c3-4f8c-a688-7e91ab3b22ce',
+  'Rolling in the Deep',
+  'Adele',
+  '21',
+  2010,
+  228,
+  'Pop'
+);
+
+-- Song 9
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  'f02e73c3-d6bb-4e4b-b9ec-731276c69f0d',
+  'Stairway to Heaven',
+  'Led Zeppelin',
+  'Led Zeppelin IV',
+  1971,
+  482,
+  'Rock'
+);
+
+-- Song 10
+INSERT INTO song (id, name, artist, album, year, song_length_secs, genre)
+VALUES (
+  '320ef126-df23-4295-8cf9-74d50e4a5db7',
+  'I Will Always Love You',
+  'Whitney Houston',
+  'The Bodyguard',
+  1992,
+  273,
+  'Pop'
+);
